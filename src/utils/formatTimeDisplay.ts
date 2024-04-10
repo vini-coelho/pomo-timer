06a2a5e2d30e0f636ba70e7068e4b43f4ff9b59f
Vinicius Coelho <50638499+vini-coelho@users.noreply.github.com>
2024-04-10 13:30:00 -0300

    add 2

--- src/utils/formatTimeDisplay.ts
+++ src/utils/formatTimeDisplay.ts
@@ -1,8 +1,8 @@
 const formatTimer: (seconds: number) => string = (seconds: number) => {
-  const min = Math.floor(seconds / 60);
-  const sec = Math.floor(seconds % 60);
+  const min2 = Math.floor(seconds / 60);
+  const sec2 = Math.floor(seconds % 60);
 
-  return `${min < 9 ? `0${min}` : min}:${sec < 9 ? `0${sec}` : sec}`;
+  return `${min2 < 9 ? `0${min2}` : min2}:${sec2 < 9 ? `0${sec2}` : sec2}`;
 };
 
 export default formatTimer;
